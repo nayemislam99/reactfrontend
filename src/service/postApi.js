d9148f3da469f3e5bb6927c2f3d8330a46755aa0
Nayem Islam <Khannayem206@gmail.com>
2023-05-07 09:51:58 +0600

    base url update

--- src/service/postApi.js
+++ src/service/postApi.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 export const postApi = axios.create({
-  baseURL: 'https://server.dosbubble.com/api/post',
+  baseURL: 'https://spiky-wave-production.up.railway.app/api/post',
 });
 
 export const userApi = axios.create({
-  baseURL: 'https://server.dosbubble.com/api/user',
+  baseURL: 'https://spiky-wave-production.up.railway.app/api/user',
 });
